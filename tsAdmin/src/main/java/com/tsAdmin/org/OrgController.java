e5be5b35b39bf6715ddad3a07ad202cd2342769a
zyy-arch <894800513@qq.com>
2025-10-27 17:19:38 +0800

    version-1
    实现功能：
    1.数据库通过API获取POI点（手动）
    2.在viewmap界面能够显示出POI点

--- tsAdmin/src/main/java/com/tsAdmin/org/OrgController.java
+++ tsAdmin/src/main/java/com/tsAdmin/org/OrgController.java
@@ -6,6 +6,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.List;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
@@ -18,6 +19,7 @@ import com.jfinal.aop.Clear;
 import com.jfinal.core.Controller;
 import com.jfinal.json.Json;
 import com.jfinal.kit.HttpKit;
+import com.jfinal.kit.Ret;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
@@ -61,8 +63,8 @@ public class OrgController extends Controller {
 		String para1=getPara("loginName");
 		String para2=getPara("pass");
 		System.out.println(para1+"\\"+para2);
-		String mykeyString="680174f12400afe6abc38c1c20d3cca1";
-		
+		String mykeyString="af8d7eef6373429b855d68efaf3ed6af";
+		int para3 = 5;
 		
 		//转换为URL编码用于传递中文
 		try {
@@ -73,61 +75,51 @@ public class OrgController extends Controller {
 			e.printStackTrace();
 		}
 		
-		String RequestURL="https://restapi.amap.com/v3/place/text?"
+		String FirstURL="https://restapi.amap.com/v3/place/text?"
 				+ "keywords="+para2+"&city="+para1+"&offset=20&page=1&key="+mykeyString+"&extensions=all";
-		String resText=HttpKit.get(RequestURL);
-		//解析并存储到数据库
-		//1先解析
-		JSONObject resOBJ=JSONObject.parseObject(resText);//把文本格式的json字符串转换为对象，便于取值
-		JSONArray pois=resOBJ.getJSONArray("pois");
+
+		String resText = HttpKit.get(FirstURL);
 		
-		//pois中，数据是array形式，所以用for循环
-		for (int i = 0; i < pois.size(); i++) {
-			String objString=pois.get(i).toString();//获得返回结果集中，第i个对象的string文本。
-			JSONObject singleObj= JSONObject.parseObject(objString);//将对象由string重新转换为jsonObj
-			String POI_name=singleObj.get("name").toString();//两种方式，一种是先get一个object，再转String，另一种是:getString
-			String POI_location=singleObj.get("location").toString();
-			String locationID=singleObj.getString("id");
-			//由于经纬度是拼接在一个字符串中的，需要分割
-			String POI_lat=POI_location.split(",")[0];//取分割后的第0个
-			String POI_lon=POI_location.split(",")[1];//取分割后的第1个
-			//系统调试输出
-			System.out.println("ID:"+locationID+"地点是："+POI_name+",经度:"+POI_lat+"，纬度:"+POI_lon);
-			//接下来根据locationID查询数据库中是否有存储或，若没有则使用DB.save()方法插入一行数据。
-			String New_id="";
-			if (locationID.equals(New_id)) {//判断两个字符是否一致
+		for(int j = 1; j <= para3; j++) {
+			String RequestURL="https://restapi.amap.com/v3/place/text?"
+					+ "keywords="+para2+"&city="+para1+"&offset=20&page="+j+"&key="+mykeyString+"&extensions=all";
+			resText=HttpKit.get(RequestURL);
+			//解析并存储到数据库
+			//1先解析
+			JSONObject resOBJ=JSONObject.parseObject(resText);//把文本格式的json字符串转换为对象，便于取值
+			JSONArray pois=resOBJ.getJSONArray("pois");
+			
+			//pois中，数据是array形式，所以用for循环
+			for (int i = 0; i < pois.size(); i++) {
+				String objString=pois.get(i).toString();//获得返回结果集中，第i个对象的string文本。
+				JSONObject singleObj= JSONObject.parseObject(objString);//将对象由string重新转换为jsonObj
+				String POI_name=singleObj.get("name").toString();//两种方式，一种是先get一个object，再转String，另一种是:getString
+				String POI_location=singleObj.get("location").toString();
+				String locationID=singleObj.getString("id");
+				//由于经纬度是拼接在一个字符串中的，需要分割
+				String POI_lat=POI_location.split(",")[0];//取分割后的第0个
+				String POI_lon=POI_location.split(",")[1];//取分割后的第1个
+				//系统调试输出
+				System.out.println("ID:"+locationID+"地点是："+POI_name+",经度:"+POI_lat+"，纬度:"+POI_lon);
+				//接下来根据locationID查询数据库中是否有存储或，若没有则使用DB.save()方法插入一行数据。
+				
+				Record test_poiRecord=new Record();
+				test_poiRecord.set("poi_name", POI_name).set("poi_lat", POI_lat);
+				
+				test_poiRecord.set("poi_lon", POI_lon);
+				test_poiRecord.set("poi_type", "1");
+				test_poiRecord.set("poi_status", "1");
+				test_poiRecord.set("create_time", tools.getDataTimeString());
+				Db.save("poi", test_poiRecord);
+				
+//				String New_id="";
+//				if (locationID.equals(New_id)) {//判断两个字符是否一致
+//					
+//				}
+				
 			}
-			
-			
-			
-			/*（已成功实现，用来将非重复数据插入数据库）
-			// 去掉 locationID 相关逻辑，适配 poi_type 整数类型
-			Record poiRecord = new Record();
-			poiRecord.set("poi_name", POI_name);      // 地点名称（字符串类型，正常赋值）
-			poiRecord.set("poi_lat", POI_lat);        // 纬度（根据表结构，若为字符串则直接用，若为数值需转成 Double）
-			poiRecord.set("poi_lon", POI_lon);        // 经度（同纬度，注意类型匹配）
-			// 关键修改：poi_type 用整数赋值（如 0 代表“默认类型”，1 代表“餐饮”等，根据你的业务定义）
-			poiRecord.set("poi_type", 0);             // 整数 0 替代字符串“默认类型”，适配表的 INT 类型
-			poiRecord.set("poi_status", 1);           // 状态也用整数（1 代表有效，符合整数类型）
-			poiRecord.set("create_time", tools.getDataTimeString()); // 创建时间（字符串，正常赋值）
-
-			// （可选）去重逻辑：通过“名称+经纬度”判断（避免重复插入）
-			Record existingRecord = Db.findFirst(
-			    "select * from e_poi where poi_name = ? and poi_lat = ? and poi_lon = ?",
-			    POI_name, POI_lat, POI_lon
-			);
-
-			if (existingRecord == null) {
-			    Db.save("e_poi", poiRecord); // 此时所有字段类型与表匹配，可正常插入
-			    System.out.println("插入新POI记录：" + POI_name + "（类型：默认类型，对应值：0）");
-			} else {
-			    System.out.println("POI记录已存在，跳过插入：" + POI_name);
-			}
-			*/
-			
-			
-			
 		}
+		
 		//组装为json结构，用于前端显示
 		JSONObject resJO=new JSONObject();
 		resJO.put("resText", resText);
@@ -141,25 +133,40 @@ public class OrgController extends Controller {
 	public void viewmap() {
 		//空接口（暂时没有数据传递）
 	}
+	
+	@Clear
+    public void viewMapPoint() {
+		try {
+	        String sql = "SELECT poi_name, poi_type, poi_lat, poi_lon, create_time FROM poi WHERE poi_status = 1";
+	        List<Record> poiList = Db.find(sql);
+	        
+	        List<Map<String, Object>> resultList = new ArrayList<>();
+            for (Record record : poiList) {
+                Map<String, Object> poiMap = new HashMap<>();
+                poiMap.put("poi_lat", record.getDouble("poi_lat"));
+                poiMap.put("poi_lon", record.getDouble("poi_lon"));
+                resultList.add(poiMap);
+            }
+            
+	        JSONObject resJO=new JSONObject();
+			resJO.put("data", resultList);
+			resJO.put("code", 1);
+			renderJson(resJO);
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        // 返回错误响应
+	        renderJson(Ret.fail("code", -1).set("message", "数据查询失败").set("data", new ArrayList<>()));
+	    }
+    }
+	
+	
 	/**
 	 * 登录页面
 	 */
 	@Clear
 	public void login() {
-		//插入数据
-		Record e_poiRecord=new Record();
-		e_poiRecord.set("poi_name", "九里堤加油站2").set("poi_lat", "104");
-		
-		e_poiRecord.set("poi_lon", "31");
-		e_poiRecord.set("poi_type", "1");
-		e_poiRecord.set("poi_status", "1");
-		e_poiRecord.set("create_time", tools.getDataTimeString());//
-		Db.save("e_poi", e_poiRecord);
-		
-		
-		
 		//查询
-		String thisTableString="e_poi";
+		String thisTableString="poi";
 		String sql="select * from "+thisTableString;//查询语句构造
 		List<Record>resList=Db.find(sql);
 		for (int i = 0; i < resList.size(); i++) {
@@ -167,7 +174,6 @@ public class OrgController extends Controller {
 			System.out.println(thisName);
 		}
 		setAttr("resList", resList);//把数据传递到前端供获取
-		
 	}
 	@Clear
 	public void testLLM() {
